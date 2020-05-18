790e1f34a94a79902229429ee8904ac8f982cf84
LenaGritse <65347294+LenaGritse@users.noreply.github.com>
2020-05-18 23:31:35 +0400

    HomeWork2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 //тест заданий
-        System.out.println(three(4,6,23,78));
+       /* System.out.println(three(4,6,23,78));
         System.out.println(four(4,5));
         five(73);
         System.out.println(six(73));
         hello("Маша");
-        whichYear(1265);
+        whichYear(1265);*/
+        //ДОМАШНЯЯ РАБОТА №2
+        invertArray();
+        fillArray();
+        changeArray();
+        fillDiagonal();
+        minMax();
+        int[] arr = {3, 5, 4, 4};
+        System.out.println(checkBalance(arr));
         //задание №2
-        byte a;
+       /* byte a;
         a = -120;
         short b;
         b = 12442;
@@ -23,51 +33,140 @@ public class Main {
         char g;
         g = 1067;
         boolean h;
-        h = false;
+        h = false;*/
     }
+
     //задание №3
     public static int three(int a, int b, int c, int d) {
         return a * (b + (c / d));
     }
+
     //задание №4
-    public static  boolean four(int a, int b) {
+    public static boolean four(int a, int b) {
         int sum = a + b;
         if (sum >= 10 && sum <= 20) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
+
     //задание №5
     public static void five(int a) {
         if (a % 2 == 0 || a == 0) {
             System.out.println("Число положительное");
-        }
-        else {
+        } else {
             System.out.println("Число отрицательное");
         }
     }
+
     //задание №6
     public static boolean six(int a) {
         if (a < 0) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
+
     //задание №7
     public static void hello(String name) {
         System.out.println("Привет, " + name + "!");
     }
+
     //задание №8
     public static void whichYear(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("Год високосный");
-        }
-        else {
+        } else {
             System.out.println("Год невисокосный");
         }
+    }
+
+    //ДОМАШНЯЯ РАБОТА №2
+    //задание №1
+    public static void invertArray() {
+        int[] arr = {1, 1, 1, 1, 1, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //задание №2
+    public static void fillArray() {
+        int[] arr = new int[8];
+        for (int i = 0, j = 0; i < arr.length; i++) {
+            arr[i] = j;
+            j = j + 3;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    //задание №3
+    public static void changeArray() {
+        int[] w = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < w.length; i++) {
+            if (w[i] < 6) {
+                w[i] = w[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(w));
+    }
+
+    //задание №4
+    public static void fillDiagonal() {
+        int[][] arr = new int[4][4];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0, j2 = arr[i].length; j < arr[i].length; j++, j2--) {
+                if(i == j || i == (j2 - 1)) {
+                    arr[i][j] = 1;
+                }
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.println();
+        }
+    }
+
+    //задание №5
+    public static void minMax() {
+        int[] arr = {12, 84, 30, 55, 8, 96};
+        int min;
+        min = arr[0];
+        int max;
+        max = min;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println("Минимальное значение - " + min + ", " + "максимальное - " + max);
+    }
+
+    //задание №6
+    private static boolean checkBalance(int[] arr) {
+        int a;
+        int b;
+        for (int i = 0; i < arr.length + 1; i++) {
+            a = 0;
+            b = 0;
+            for (int j = 0; j < i; j++) {
+                a += arr[j];
+            }
+            for (int j = i; j < arr.length; j++) {
+                b += arr[j];
+            }
+            if (a == b) {
+                return true;
+            }
+        }
+        return false;
     }
 }
