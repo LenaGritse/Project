2b92e0a5f0b2518c1fad991ac0ec3d468eae57ef
LenaGritse <65347294+LenaGritse@users.noreply.github.com>
2020-05-26 16:38:13 +0400

    HomeWork4

--- src/Main.java
+++ src/Main.java
@@ -212,4 +212,150 @@ public class Main {
         while (sc.nextInt() == 1);
     }
 
+    //ДОМАШНЯЯ РАБОТА №4
+    public static class XOGame {
+        static final int SIZE = 3;
+        static final int DOTS_TO_WIN = 3;
+
+        static final char DOT_X = 'X';
+        static final char DOT_O = 'O';
+        static final char DOT_EMPTY = '.';
+
+        static char[][] map;
+
+        static Scanner sc = new Scanner(System.in);
+        static Random random = new Random();
+
+        public static void main(String[] args) {
+            initMap();
+            printMap();
+
+            while (true) {
+                humanTurn();
+                printMap();
+
+                if (checkWin(DOT_X)) {
+                    System.out.println("Вы выиграли!");
+                    break;
+                }
+
+                if (isFull()) {
+                    System.out.println("Ничья");
+                    break;
+                }
+
+                aiTurn();
+                printMap();
+
+                if (checkWin(DOT_O)) {
+                    System.out.println("Компьютер победил!");
+                    break;
+                }
+
+                if (isFull()) {
+                    System.out.println("Ничья");
+                    break;
+                }
+            }
+        }
+
+        public static void initMap() {
+            map = new char[SIZE][SIZE];
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    map[i][j] = DOT_EMPTY;
+                }
+            }
+        }
+
+        public static void printMap() {
+            System.out.print("  ");
+            for (int i = 0; i < SIZE; i++) {
+                System.out.print(i + 1 + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < SIZE; i++) {
+                System.out.print(i + 1 + " ");
+                for (int j = 0; j < SIZE; j++) {
+                    System.out.print(map[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        public static void humanTurn() {
+            int x, y;
+
+            do {
+                System.out.println("Введите координаты Вашего хода X Y");
+                x = sc.nextInt() - 1;
+                y = sc.nextInt() - 1;
+            } while (!isCellValid(y, x));
+            map[y][x] = DOT_X;
+        }
+
+        public static boolean isCellValid(int y, int x) {
+            if (x < 0 || y < 0 || x >= SIZE || y >= SIZE) {
+                return false;
+            }
+            return map[y][x] == DOT_EMPTY;
+        }
+
+        public static void aiTurn() {
+            int x, y;
+
+            do {
+                x = random.nextInt(SIZE);
+                y = random.nextInt(SIZE);
+            } while (!isCellValid(y, x));
+            map[y][x] = DOT_O;
+        }
+
+        public static boolean isFull() {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (map[i][j] == DOT_EMPTY) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static boolean checkWin(char c) {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (checkLine(i, j, 0, 1, c)) {
+                        return true;
+                    }
+                    if (checkLine(i, j, 1, 1, c)) {
+                        return true;
+                    }
+                    if (checkLine(i, j, 1, 0, c)) {
+                        return true;
+                    }
+                    if (checkLine(i, j, -1, 1, c)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static boolean checkLine(int y, int x, int lineY, int lineX, char c) {
+            int X = x + (DOTS_TO_WIN - 1) * lineX;
+            int Y = y + (DOTS_TO_WIN - 1) * lineY;
+            if (X < 0 || Y < 0 || X > SIZE - 1 || Y > SIZE - 1) {
+                return false;
+            }
+            for (int i = 0; i < DOTS_TO_WIN; i++) {
+                int a = y + i * lineY;
+                int b = x + i * lineX;
+                if (map[a][b] != c) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
